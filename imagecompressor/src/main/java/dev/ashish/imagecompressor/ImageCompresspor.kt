26397a8fd6337a937ef0b168491e84a2556b7350
ashish <ashish@stackfusion.io>
2024-02-26 20:55:45 +0530

    implementing the coroutine for the operation

--- imagecompressor/src/main/java/dev/ashish/imagecompressor/ImageCompresspor.kt
+++ imagecompressor/src/main/java/dev/ashish/imagecompressor/ImageCompresspor.kt
@@ -1,65 +1,50 @@
 package dev.ashish.imagecompressor
 
+//noinspection ExifInterface
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.Canvas
 import android.graphics.Matrix
-import android.graphics.Paint
-//noinspection ExifInterface
 import android.media.ExifInterface
 import android.os.Build
-import android.os.Environment
 import android.util.Log
 import androidx.annotation.RequiresApi
-import java.io.File
-import java.io.FileNotFoundException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.withContext
 import java.io.FileOutputStream
 import java.io.IOException
-import java.nio.file.Files
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
-import kotlin.math.roundToInt
 
 
 @RequiresApi(Build.VERSION_CODES.O)
 object ImageCompressor {
-    fun compressImage(imagePath: String, imageQuality: Int = 50): String {
-        val file = File(imagePath)
-        if (!file.exists()) {
-            return ""
-        }
+    suspend fun compressImage(
+        imagePath: String,
+        imageQuality: Int = 50,
+    ) {
+        withContext(Dispatchers.IO) {
+            val timeStamp: String =
+                SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+            val imageName = "IMG_$timeStamp.jpg"
+            val filePath = getOutputMediaFile(imageName)!!.absolutePath
 
-        val timeStamp: String =
-            SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
-        val imageName = "IMG_$timeStamp.jpg"
-
-        val filePath = getOutputMediaFile(imageName)!!.absolutePath
-
-        try {
             val options = BitmapFactory.Options()
             options.inJustDecodeBounds = true
             BitmapFactory.decodeFile(imagePath, options)
 
             val reqWidth = 1024
             val reqHeight = 912
-
-            val actualWidth = options.outWidth
-            val actualHeight = options.outHeight
-
-            // Calculate inSampleSize for downscaling
             options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight)
-
             options.inJustDecodeBounds = false
             options.inPurgeable = true
             options.inInputShareable = true
             options.inTempStorage = ByteArray(16 * 1024)
 
             val bitmap = BitmapFactory.decodeFile(imagePath, options)
-            val scaledBitmap = Bitmap.createScaledBitmap(
-                bitmap, reqWidth, reqHeight, true
-            )
-            bitmap.recycle() // Release memory used by the original bitmap
+            val scaledBitmap = Bitmap.createScaledBitmap(bitmap, reqWidth, reqHeight, true)
+            bitmap.recycle()
 
             val exifOrientation = getExifOrientation(imagePath)
             val rotatedBitmap = rotateBitmap(scaledBitmap, exifOrientation)
@@ -67,12 +52,8 @@ object ImageCompressor {
             val out = FileOutputStream(filePath)
             rotatedBitmap.compress(Bitmap.CompressFormat.JPEG, imageQuality, out)
             out.close()
-            rotatedBitmap.recycle() // Release memory used by the scaled bitmap
-
-            return filePath
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return ""
+            rotatedBitmap.recycle()
+            filePath
         }
     }
 
